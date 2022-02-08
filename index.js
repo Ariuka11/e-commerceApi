3dd040313cdd841bc7bcc7acbc5aaccdd9426b8e
ariuka11 <ariun1118@gmail.com>
2022-02-08 10:03:13 -0500

    fixed cors issue

--- index.js
+++ index.js
@@ -19,8 +19,23 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const domainsFromEnv = process.env.CORS_DOMAINS || "";
 
-app.use(cors());
+const whitelist = domainsFromEnv.split(",").map((item) => item.trim());
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Working");
